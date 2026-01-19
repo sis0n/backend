e237c1699746899ac75bf7e43db0cdec550fa146
alwyn <adrianoalwyn@gmail.com>
2026-01-19 23:02:38 +0800

    downloaded the passport laravel.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you can register web routes for your application. These
+| routes are typically used for rendering traditional web pages.
+|
+| For API routes, please refer to 'routes/api.php'.
+|
+*/
